d960ba1b59c8778bf3b3865870ecef0dc9e8523a
Maria <mashero4ka@gmail.com>
2020-11-16 00:06:51 +0200

    devide into parts-function

---
+++ src/js/fetchCountries.js
@@ -0,0 +1,84 @@
+import { error } from '@pnotify/core';
+
+import countryCardTpl from '../templates/countryCard.hbs';
+import countryListTpl from '../templates/countryList.hbs';
+
+
+const debounce = require('lodash.debounce');
+
+const baseUrl = 'https://restcountries.eu/rest/v2/name/';
+
+const inputRef = document.querySelector('.form-input');
+const resultRef = document.querySelector('.search-result');
+const fullName = '?fullText=true';
+
+
+
+function getRestData(countryName) {
+  resultRef.textContent = countryName;
+   const countryNameAPItoFetch = false
+    ? baseUrl + countryName + fullName
+    : baseUrl + countryName;
+  return fetch(countryNameAPItoFetch).then(response => {
+    if (response.ok || response.status === 404) return response.json();
+    // throw new Error('Error fetching data');
+  }).catch(err => console.log(err));
+}
+
+function searchData(event) {
+ removeError();
+  const countryName = event.target.value.trim();
+  if (countryName.length === 0) return clearResult();
+  fetchData(countryName);
+}
+
+function operateData(data) {
+  clearResult();
+  if (data.message) return showError(data.message);
+  if (data.length > 10)
+    return showError('Too many matches found. Please enter more specific query');
+  if (data.length === 1) return (resultRef.innerHTML = countryCardTpl(data));
+  if (data.length > 1) resultListListener(data);
+}
+
+function clearResult() {
+  resultRef.innerHTML = '';
+  removeError();
+}
+
+function removeError() {
+  const errorRef = document.querySelector('.error');
+  if (errorRef) errorRef.remove();
+}
+
+function resultListListener(data) {
+  resultRef.innerHTML = countryListTpl(data);
+  const listRef = document.querySelector('.country-list');
+  listRef.addEventListener('click', resultListenToFetch, { once: true });
+}
+
+function resultListenToFetch(event) {
+  inputRef.value = event.target.textContent;
+  fetchData(inputRef.value);
+}
+
+function fetchData(countryName) {
+  return getRestData(countryName)
+    .then(operateData)
+    .catch(err => {
+      error({
+        text: `${err}`,
+        type: 'error',
+      });
+    });
+}
+
+
+function showError(err = 'Error') {
+    error({
+        text: `${err}`,
+        type: 'error',
+    });
+  }
+
+inputRef.addEventListener('input', debounce(searchData, 500));
---
+++ src/js/pnotify.js
@@ -0,0 +1,8 @@
+import { defaults } from '@pnotify/core';
+
+defaults.delay = '1000';
+defaults.closer = false;
+defaults.sticker = false;
+defaults.hide = true;
+defaults.addClass = 'error';
+defaults.autoOpen = false;
